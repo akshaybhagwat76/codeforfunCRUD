1d64174f26916725a17ab9e4f9753d437aac54b1
YoloWarrior <tema-sid@mail.ru>
2020-04-29 06:18:35 +0300

    fix

--- CodeForFun.UI.WebMvcCore/Controllers/ProductsToCustomerController.cs
+++ CodeForFun.UI.WebMvcCore/Controllers/ProductsToCustomerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CodeForFun.Repository.Business.Abstract.Services;
 using CodeForFun.Repository.Entities.Concrete;
 using CodeForFun.UI.WebMvcCore.Models.ViewModels;
@@ -17,12 +18,15 @@ namespace CodeForFun.UI.WebMvcCore.Controllers
 		private readonly IProductsToCustomer _productsToCustomer;
 		private readonly ICustomerService _CustomerService;
 		private readonly IProductService _productService;
+		private readonly IMapper _mapper;
 
-		public ProductsToCustomerController(IProductsToCustomer productsToCustomer, ICustomerService CustomerService, IProductService productService)
+		public ProductsToCustomerController(IProductsToCustomer productsToCustomer, ICustomerService CustomerService, 
+			IProductService productService,IMapper mapper)
 		{
 			_productsToCustomer = productsToCustomer;
 			_CustomerService = CustomerService;
 			_productService = productService;
+			_mapper = mapper;
 		}
 
 		// GET: api/ProductsToCustomer
@@ -53,6 +57,39 @@ namespace CodeForFun.UI.WebMvcCore.Controllers
 			{
 				return BadRequest("Some Error while add Products To Customer");
 			}
+			return Ok();
+		}
+
+		[HttpPut]
+		public IActionResult Put([FromBody] ProductsToCustomerViewModel productsToCustomer)
+		{
+			try
+			{
+				var productToCustomer = _productsToCustomer.GetAsync(productsToCustomer.Id).Result;
+
+				productToCustomer.CustomerId = productToCustomer.CustomerId;
+				productToCustomer.ProductId = productToCustomer.ProductId;
+
+				_productsToCustomer.UpdateAsync(productToCustomer);
+
+			}
+			catch (Exception ex)
+			{
+				return BadRequest("Some Error while add Products To Customer");
+			}
+			return Ok();
+		}
+
+		[HttpDelete]
+		public IActionResult Delete(int id)
+		{
+			var getProd = _productsToCustomer.GetAsync(id);
+
+			if (getProd.Result != null)
+			{
+				_productsToCustomer.DeleteAsync(getProd.Result);
+			}
+
 			return Ok();
 		}
 
